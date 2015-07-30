c777b50a2515bba82dd27f41c5dbfbca1f298a12
John <john@yoga>
2015-07-31 02:37:47 +0300

    Fixed CommandsTest to pass on Android emulator

--- HttpUart/app/src/androidTest/java/com/httpuart/CommandsTest.java
+++ HttpUart/app/src/androidTest/java/com/httpuart/CommandsTest.java
@@ -5,7 +5,6 @@ import junit.framework.TestCase;
 public class CommandsTest extends TestCase {
     public void testQuadrobotCommands() throws Exception {
         Commands.Map map = Commands.quadrobot();
-        assertTrue(false);
         assertTrue(map.containsKey("s"));
         assertTrue(map.containsKey("f"));
     }
