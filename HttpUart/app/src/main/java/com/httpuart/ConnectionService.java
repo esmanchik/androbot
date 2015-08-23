2332b93a7ebbfffd7486fd0a818483e1d83b06cd
John <john@yoga>
2015-08-23 20:46:51 +0300

    Draft to send camera image over HTTP

--- HttpUart/app/src/main/java/com/httpuart/ConnectionService.java
+++ HttpUart/app/src/main/java/com/httpuart/ConnectionService.java
@@ -1,20 +1,31 @@
 package com.httpuart;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -30,17 +41,34 @@ public class ConnectionService extends Service {
     private Thread thread = null;
     private ServerSocket server = null;
     private Commands commands;
+    private Handler handler;
+    private byte[] picture = null;
 
     @Override
     public void onCreate() {
         logOpen();
-        dbg("connection service created");
         super.onCreate();
+        dbg("connection service created");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         try {
+            handler = new Handler(Looper.getMainLooper()) {
+                @Override
+                public void handleMessage(Message message) {
+                    // This is where you do your work in the UI thread.
+                    // Your worker tells you in the message what to do.
+                    try {
+                        takePhoto();
+                    } catch (Exception e) {
+                        xcpt(e);
+                    }
+                }
+            };
+            thread = connectionThread(intent.getExtras().getInt(PORT));
+            thread.start();
+            dbg("connection service started");
             boolean bt = intent.getExtras().getString(UART).equals(UART_BLUETOOTH);
             uart = bt ? new BlueUart() : new UsbUart(this);
             uart.open();
@@ -53,9 +81,6 @@ public class ConnectionService extends Service {
                 parsed += command + " ";
             }
             dbg("Parsed commands " + parsed);
-            thread = connectionThread(intent.getExtras().getInt(PORT));
-            thread.start();
-            dbg("connection service started");
         } catch(Exception e) {
             xcpt(e);
         }
@@ -149,6 +174,8 @@ public class ConnectionService extends Service {
         String response = "HTTP/1.0 404 Not Found\r\n\r\nNot Found";
         if (request.startsWith("GET /favicon.ico HTTP")) {
             // Not Found
+        } else if (request.startsWith("GET /camera HTTP")) {
+            response = camera();
         } else {
             for(String command: commands.available()) {
                 if (request.startsWith("GET /" + command)) {
@@ -175,6 +202,117 @@ public class ConnectionService extends Service {
             xcpt(e);
         }
         return request;
+    }
+
+    private String camera() {
+        synchronized (handler) {
+            picture = null;
+        }
+        handler.obtainMessage().sendToTarget();
+        dbg("Send message to LooperThread");
+        byte[] picture = null;
+        for (int i = 0; picture == null && i < 30; i++) {
+            SystemClock.sleep(100);
+            synchronized (handler) {
+                picture = this.picture;
+            }
+            dbg("Try "+ i +": picture is " + picture);
+        }
+        String response = "HTTP/1.0 500 Internal System Error\r\n\r\n";
+        if (picture != null) {
+            String length = Integer.toString(picture.length);
+            dbg("Got picture of " + length + " bytes");
+            String decoded = null;
+            try {
+                decoded = new String(picture, /*"ASCII"*/"ISO-8859-1");
+                response = "HTTP/1.0 200 OK\r\nContent-Type: image/jpeg\r\n" +
+                        "Content-Length: " + decoded.length() + "\r\n\r\n" + decoded;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return response;
+    }
+
+    @SuppressWarnings("deprecation")
+    private void takePhoto() {
+        final SurfaceView preview = new SurfaceView(this);
+        SurfaceHolder holder = preview.getHolder();
+        // deprecated setting, but required on Android versions prior to 3.0
+        // holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            //The preview must happen at or after this point or takePicture fails
+            public void surfaceCreated(SurfaceHolder holder) {
+                dbg("Surface created");
+
+                Camera camera = null;
+
+                try {
+                    camera = Camera.open();
+                    dbg("Opened camera");
+
+                    try {
+                        camera.setPreviewDisplay(holder);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    camera.startPreview();
+                    dbg("Started preview");
+
+                    camera.takePicture(null, null, new Camera.PictureCallback() {
+                        @Override
+                        public void onPictureTaken(byte[] data, Camera camera) {
+                            dbg("Took picture of " + Integer.toString(data.length) + " bytes");
+                            spit("httpuart.jpg", data);
+                            dbg("Saved picture to file");
+                            synchronized (handler) {
+                                ConnectionService.this.picture = data;
+                            }
+                            dbg("Saved picture to property");
+                            camera.release();
+                        }
+                    });
+                } catch (Exception e) {
+                    if (camera != null)
+                        camera.release();
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+        });
+
+        WindowManager wm = (WindowManager)this
+                .getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                1, 1, //Must be at least 1x1
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                0,
+                //Don't know if this is a safe default
+                PixelFormat.UNKNOWN);
+
+        //Don't set the preview visibility to GONE or INVISIBLE
+        wm.addView(preview, params);
+    }
+
+    private void spit(String fileName, byte[] data) {
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+            FileOutputStream stream = new FileOutputStream(file);
+            stream.write(data);
+            stream.close();
+        } catch(Exception e) {
+            xcpt(e);
+        }
     }
 
     void logOpen() {
